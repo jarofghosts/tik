c1ddf603dd4d6f6f308ca32a4a834336cecbe464
jesse keane <me@jessekeane.me>
2013-10-07 08:23:14 -0700

    bleh

--- index.js
+++ index.js
@@ -9,73 +9,76 @@ var c = require('commander'),
     fs = require('fs'),
     stream = require('stream'),
     dir = path.resolve(process.env.HOME || process.env.USERPROFILE, '.tik'),
-    isCli = (require.main === module);
+    isCli = (require.main === module)
 
-if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+try {
+  fs.mkdirSync(dir)
+} catch (e) {
+}
 
-module.exports.Tik = Tik;
-module.exports.createTik = createTik;
+module.exports.Tik = Tik
+module.exports.createTik = createTik
 
 function Tik(settings) {
   
-  if (!(this instanceof Tik)) return new Tik(settings);
+  if (!(this instanceof Tik)) return new Tik(settings)
 
-  this.settings = settings || {};
-  this.settings.db = this.settings.db || dir + '/db';
-  this.db = levelup(path.normalize(this.settings.db));
+  this.settings = settings || {}
+  this.settings.db = this.settings.db || path.join(dir, 'db')
+  this.db = levelup(path.normalize(this.settings.db))
 
-  return this;
+  return this
 }
 
 Tik.prototype.listAll = function () {
   var tr = through(),
-      db = this.db;
+      db = this.db
 
-  process.nextTick(go);
-  return tr;
+  process.nextTick(go)
+  return tr
 
   function go() {
     db.createReadStream()
       .on('data', function (data) {
-        if (!data.key || !data.value) return;
-        tr.queue([color.green(data.key + ':'), data.value].join(' '));
+        if (!data.key || !data.value) return
+        tr.queue([color.green(data.key + ':'), data.value].join(' '))
       })
       .on('end', closeStream)
-      .on('close', closeStream);
+      .on('close', closeStream)
   }
 
-  function closeStream() { tr.queue(null); }
+  function closeStream() { tr.queue(null) }
 };
 
 Tik.prototype.keyStream = function () {
-  return this.db.createKeyStream();
-};
+  return this.db.createKeyStream()
+}
 
 Tik.prototype.readStream = function () {
   var tr = through(write, function () {}),
-      db = this.db;
+      db = this.db
 
-  return tr;
+  return tr
 
   function write(buf) {
-    var keyName = buf.toString();
+    var keyName = buf.toString()
     db.get(keyName, function (err, data) {
-      if (err) tr.queue(null);
-      if (data) tr.queue(data);
-    });
+      if (err) tr.queue(null)
+      if (data) tr.queue(data)
+    })
   }
-};
+}
 
 Tik.prototype.writeStream = function () {
-  return this.db.createWriteStream({ type: 'put' });
-};
+  return this.db.createWriteStream({ type: 'put' })
+}
 
 Tik.prototype.deleteStream = function () {
-  return this.db.createWriteStream({ type: 'del' });
-};
+  return this.db.createWriteStream({ type: 'del' })
+}
 
 function createTik(settings) {
-  return new Tik(settings);
+  return new Tik(settings)
 }
 
 if (isCli) {
@@ -89,24 +92,24 @@ if (isCli) {
       var delStream = new Tik({ db: c.database }).deleteStream(),
           args = Array.prototype.slice.call(arguments, 0, -2),
           i = 0,
-          l = args.length;
+          l = args.length
       for (; i < l; ++i) {
-        delStream.write({ key: args[i] });
+        delStream.write({ key: args[i] })
       }
-      delStream.end();
-    });
+      delStream.end()
+    })
   c
     .command('ls')
     .description('list all items')
     .action(function () {
-      new Tik({ db: c.database }).listAll().pipe(appendage({ after: '\n' })).pipe(process.stdout);
-     });
+      new Tik({ db: c.database }).listAll().pipe(appendage({ after: '\n' })).pipe(process.stdout)
+     })
   c
     .command('lskeys')
     .description('list all keys')
     .action(function () {
-      new Tik({ db: c.database }).keyStream().pipe(appendage({ after: '\n' })).pipe(process.stdout);
-    });
+      new Tik({ db: c.database }).keyStream().pipe(appendage({ after: '\n' })).pipe(process.stdout)
+    })
   c
     .command('*')
     .action(function (stuff) {
@@ -114,29 +117,29 @@ if (isCli) {
       if (c.args.length === 2) {
 
         var read = new Tik({ db: c.database }).readStream(),
-            rs = stream.Readable();
+            rs = stream.Readable()
 
-        rs.push(c.args[0]);
-        rs.push(null);
+        rs.push(c.args[0])
+        rs.push(null)
 
-        rs.pipe(read).pipe(appendage({ after: '\n' })).pipe(process.stdout);
+        rs.pipe(read).pipe(appendage({ after: '\n' })).pipe(process.stdout)
 
       } else {
 
-        c.args.pop();
+        c.args.pop()
 
         var keyName = c.args.shift(),
             keyValue = c.args.join(' '),
-            write = new Tik({ db: c.database }).writeStream();
+            write = new Tik({ db: c.database }).writeStream()
 
-        write.write({ key: keyName, value: keyValue });
-        write.end();
+        write.write({ key: keyName, value: keyValue })
+        write.end()
 
       }
 
-    });
-  c.parse(process.argv);
-  if (!c.args.length) c.help();
+    })
+  c.parse(process.argv)
+  if (!c.args.length) c.help()
 
 }
 
